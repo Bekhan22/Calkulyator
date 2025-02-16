ab6a3689a340bd9f5b2423f136c7f0288eb4d1d2
Bekhan22 <shoislom149@gmail.com>
2025-02-16 12:46:35 +0500

    Add files via upload

---
+++ mini.js
@@ -0,0 +1,10 @@
+const ekran=document.getElementById("ekran")
+function hisobla() {
+    ekran.value= eval(ekran.value);
+}
+function del(){
+    ekran.value=ekran.value.slice(0,-1);
+}
+function clearEkran(){
+    ekran.value="";
+}
